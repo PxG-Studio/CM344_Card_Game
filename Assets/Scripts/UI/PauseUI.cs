a7b596b3fe1cf0c95e2343fce30b90e0c4e7e73d
ArchitectofDoom <torea@hawaii.edu>
2025-11-12 15:24:29 -1000

    added pause menu in game

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    [SerializeField] GameObject pauseMenu;
+    public void Pause() 
+    { 
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
