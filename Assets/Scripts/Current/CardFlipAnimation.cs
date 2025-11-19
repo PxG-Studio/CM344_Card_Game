3ae0a6d571cac553e7cff3ffab3ff6eead3d3869
Archivist Bay Geld <Yaakovbengershom@gmail.com>
2025-11-18 16:52:38 -1000

    alterd flip script did an organize
    altered flip script to set the bool that determines whether a card is recognized as player 1 card or player 2 card using tags p1 and p2 tied to the frontContainer object of the card prefabs (normal and opp) instead of checking for cardmover or cardmoveropp also did an organize creating an archive folder old scripts not in use. as welll as cleaned up and renamed the folder with the in use scripts to current

--- Assets/Scripts/Current/CardFlipAnimation.cs
+++ Assets/Scripts/Current/CardFlipAnimation.cs
@@ -620,14 +620,17 @@ namespace CardGame.UI
                 }
                 
                 // Check if it's a CardMover (player card) vs CardMoverOpp (opponent card)
-                CardMover cardMover = GetComponent<CardMover>();
-                if (cardMover != null)
+                //CardMover cardMover = GetComponent<CardMover>();
+               
+                //if (cardMover != null)
+                if (frontContainer.CompareTag("p1"))
                 {
                     return true; // Player card
                 }
                 
-                CardMoverOpp cardMoverOpp = GetComponent<CardMoverOpp>();
-                if (cardMoverOpp != null)
+                //CardMoverOpp cardMoverOpp = GetComponent<CardMoverOpp>();
+                //if (cardMoverOpp != null)
+                if (frontContainer.CompareTag("p2"))
                 {
                     return false; // Opponent card
                 }
