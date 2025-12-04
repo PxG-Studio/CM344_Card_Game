ad114396b3da8adbbe7c9980594653a6df171ac4
ArchitectofDoom <torea@hawaii.edu>
2025-12-03 14:18:00 -1000

    fixed audio manager

--- Assets/Scripts/Current/Univ-Managers/Audio Manager.cs
+++ Assets/Scripts/Current/Univ-Managers/Audio Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,4 +15,15 @@ public class AudioManager : MonoBehaviour
     public AudioClip MenuSelect;
     public AudioClip CoinFlip;
     public AudioClip Tie;
+
+    private void Start()
+    {
+        musicSource.Play();
+
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 }
