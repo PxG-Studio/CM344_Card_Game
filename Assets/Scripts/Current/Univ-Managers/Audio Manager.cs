77fd5a1a4a0fc4c15c2194965c463441e4fac51e
ArchitectofDoom <torea@hawaii.edu>
2025-12-03 14:07:43 -1000

    audio manager code added

---
+++ Assets/Scripts/Current/Univ-Managers/Audio Manager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip PlayCard;
+    public AudioClip DrawCard;
+    public AudioClip FireCaptureCard;
+    public AudioClip EarthCaptureCard;
+    public AudioClip MenuSelect;
+    public AudioClip CoinFlip;
+    public AudioClip Tie;
+}
